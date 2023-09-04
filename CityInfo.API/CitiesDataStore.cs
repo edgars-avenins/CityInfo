f386dfcc83c333d83f940d59f5fbf2d31bc9da57
Edgars <edgars.avenins@gmail.com>
2023-09-05 10:08:46 +1200

    Add endpoint and dummy data

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -16,19 +16,64 @@ namespace CityInfo.API
                 {
                     Id = 1,
                     Name = "New York City",
-                    Description = "The one with big park."
+                    Description = "The one with big park.",
+                    PointOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 1,
+                            Name = "Statue of Liberty",
+                            Description = "A symbol of freeeeedom"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 2,
+                            Name = "Manhattan Park",
+                            Description = "Big Park"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
-                    Description = "Big cathedral."
+                    Description = "Big cathedral.",
+                    PointOfInterests = new List<PointOfInterestDto>
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 3,
+                            Name = "Cathedral",
+                            Description = "Almost finished"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 4,
+                            Name = "Some canals",
+                            Description = "They have water"
+                        }
+                    }
                 },
                 new CityDto()
                 {
                     Id = 3,
                     Name = "Paris",
-                    Description = "Eifel tower."
+                    Description = "Eifel tower.",
+                    PointOfInterests = new List<PointOfInterestDto>()
+                    {
+                        new PointOfInterestDto()
+                        {
+                            Id = 5,
+                            Name = "Eifel tower",
+                            Description = "Big tower"
+                        },
+                        new PointOfInterestDto()
+                        {
+                            Id = 6,
+                            Name = "Luvra",
+                            Description = "Art"
+                        }
+                    }
                 }
             };
         }
